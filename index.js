2f43318395dbac1f4aa0f219a469093b6e61c05a
iamrivera <a3m3rivera@gmail.com>
2021-07-07 08:37:51 -0700

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,21 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(time) {
+  let x = parseInt(time.split(':')[0]);
+  if (x < 12) {
+    return 'Good Morning';
+  } else if (x > 17) {
+    return 'Good Evening';
+  } else {
+    return 'Good Afternoon';
+  }
+};
+
+let greeting = greet(time);
+
+function displayMessage(greeting) {
+  let x = document.getElementById('greeting');
+  x.innerHTML = greeting; 
+};
